5f24b876c5ad3093465fe9504aa26d1529475691
yukito-takeuchi <iyggf66974@yahoo.ne.jp>
2025-12-16 13:56:00 +0900

    購入時のフォーマットエラー対応　OK

--- internal/interfaces/database/items_repository.go
+++ internal/interfaces/database/items_repository.go
@@ -172,7 +172,7 @@ func scanItem(scanner interface {
 	Scan(dest ...interface{}) error
 }) (*entity.Item, error) {
 	var item entity.Item
-	var purchaseDate string
+	var purchaseDate time.Time
 	var createdAt, updatedAt time.Time
 
 	err := scanner.Scan(
@@ -189,14 +189,7 @@ func scanItem(scanner interface {
 		return nil, err
 	}
 
-	if purchaseDate != "" {
-		if parsedDate, err := time.Parse("2006-01-02", purchaseDate); err == nil {
-			item.PurchaseDate = parsedDate.Format("2006-01-02")
-		} else {
-			item.PurchaseDate = purchaseDate
-		}
-	}
-
+	item.PurchaseDate = purchaseDate.Format("2006-01-02")
 	item.CreatedAt = createdAt
 	item.UpdatedAt = updatedAt
 
